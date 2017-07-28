f404cca373fab72a2edf115414079c0dfae4f463
0xEAB <desisma@heidel.beer>
2017-07-28 20:31:52 +0200

    Move 'ColorUtil' from <0xEAB/DCD_Parser>

---
+++ source/ColorUtil.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CoE.em8.Core.CLI
+{
+    public class ColorUtil
+    {
+        protected ConsoleColor backgroundColor;
+        protected ConsoleColor foregroundColor;
+
+        public void Restore()
+        {
+            Console.BackgroundColor = this.backgroundColor;
+            Console.ForegroundColor = this.foregroundColor;
+        }
+
+        public void Save()
+        {
+            this.backgroundColor = Console.BackgroundColor;
+            this.foregroundColor = Console.ForegroundColor;
+        }
+
+        public static void PrintWarning(params string[] warnings)
+        {
+            var cu = new ColorUtil();
+            cu.Save();
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            foreach(var w in warnings)
+                Console.WriteLine(w);
+
+            cu.Restore();
+        }
+    }
+}
+
